91f8085cd8e44d861d97b4a830c0b77dfd88324f
Faisal Iqbal <sfi2k6@outlook.com>
2017-07-06 15:09:01 +0500

    final

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+//NewV4 - get clean UUID without dashes
+//
 func NewV4() string {
 	v4 := uuid.NewV4().String()
 	return strings.Replace(v4, "-", "", -1)
