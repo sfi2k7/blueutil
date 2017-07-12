ca764f31c8198016f168bcb90564c6965ae44895
Faisal Iqbal <sfi2k6@outlook.com>
2017-07-13 01:54:27 +0500

    Compress-Uncompress

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package blueutil
 
 import (
+	"bytes"
 	"strings"
+
+	"compress/gzip"
+
+	"encoding/base64"
+
+	"io/ioutil"
 
 	"github.com/satori/go.uuid"
 )
@@ -11,4 +18,45 @@ import (
 func NewV4() string {
 	v4 := uuid.NewV4().String()
 	return strings.Replace(v4, "-", "", -1)
+}
+
+func CompressString(s string) (string, error) {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	_, err := w.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+
+	err = w.Flush()
+	if err != nil {
+		return "", err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return "", err
+	}
+
+	str := base64.StdEncoding.EncodeToString(buf.Bytes())
+	return str, nil
+}
+
+func UncompressString(s string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+
+	rdata := bytes.NewReader(data)
+	r, err := gzip.NewReader(rdata)
+	if err != nil {
+		return "", err
+	}
+
+	out, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
 }
