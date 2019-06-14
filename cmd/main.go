727e7513cb4afb0a49776341d5493eb4008b236d
Faisal Iqbal <sfi2k6@outlook.com>
2019-06-15 03:47:53 +0500

    Latest

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"fmt"
 
-	"github.com/sfi2k7/blueutil"
+	"golang.org/x/xerrors"
 )
 
+type myerror struct {
+	Name string
+}
+
+func (m *myerror) Error() string {
+	return "I had some error " + m.Name
+}
+
 func main() {
-	s := blueutil.NewMem()
-	s.Add("1", "One")
-	s2 := blueutil.NewMemWithParent(s)
-	s2.Add("2", "Two")
-	defer s.Close()
-	defer s2.Close()
-	fmt.Println(s2.Get("2"))
-	fmt.Println(s2.Get("1"))
+	e := getError()
+	var single myerror
+	ok := xerrors.As(e, &single)
+	fmt.Println(ok, single)
+	fmt.Println(e)
+	// s := blueutil.NewMem()
+	// s.Add("1", "One")
+	// s2 := blueutil.NewMemWithParent(s)
+	// s2.Add("2", "Two")
+	// defer s.Close()
+	// defer s2.Close()
+	// fmt.Println(s2.Get("2"))
+	// fmt.Println(s2.Get("1"))
+}
+
+func getError() error {
+	return &myerror{Name: "Test"}
+	// e := xerrors.New("some text")
+	// return e
 }
